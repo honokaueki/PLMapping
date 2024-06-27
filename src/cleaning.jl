05949b0ff846ca12d4e3266e7e74ee32ec2fcc0c
Garrek Stemo <8449000+garrekstemo@users.noreply.github.com>
2024-06-27 14:45:42 +0900

    flip cols not rows

--- src/cleaning.jl
+++ src/cleaning.jl
@@ -1,9 +1,8 @@
 function flip_rows(data, flip=2)
     for (i, row) in enumerate(eachcol(data))
         if i % flip == 0
-            data[i, :] = reverse(row)
+            data[:, i] = reverse(row)
         end
     end
-    println("THIS IS WORKING")
     return data
 end
